f802486d34decdbec39298f2c0f2d3ca6ceabd2b
Phil Labrum <philip.labrum@gmail.com>
2025-11-21 13:02:14 -0500

    sanity update

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,8 +1,15 @@
 import { createClient } from 'next-sanity'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || ''
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId,
+  dataset,
   apiVersion: '2024-01-01',
   useCdn: false,
 })
+
+export const isConfigured = () => {
+  return !!projectId && projectId !== ''
+}
